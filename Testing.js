3322f038ef82b605b86d59769047d935c70d1e7c
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-05-27 14:37:52 -0700

    Commit

--- Testing.js
+++ Testing.js
@@ -1,1 +1,24 @@
 // Testing JS 3
+/*
+function sayName(name) {
+
+    console.log(this);
+    return name;
+
+
+}
+*/
+
+
+//sayName("Carl");
+
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+        console.log('${this.greeting} my name is ${name}');
+        console.log(this);
+    }
+
+};
+
+myObj.sayHello('Carl');
