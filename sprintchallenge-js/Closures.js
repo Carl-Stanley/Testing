71f6d834e08f0e168d5e0bb24fa6ebb712ada92b
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-05-31 09:45:13 -0700

    Commit

--- sprintchallenge-js/Closures.js
+++ sprintchallenge-js/Closures.js
@@ -1,8 +1,8 @@
 // Closures Review.  
 // Closure function accessing item outside of function, but not visa versa. 
-
+// Items within can reach outside, but items outside cannot access inside elements. 
 // example 1 
-
+// Closures allow holding onto memory or other events for future refefence. 
 const foo = 'bar'
 
 function returnFoo () {
@@ -11,5 +11,31 @@ function returnFoo () {
 
 }
 
-returnFoo();
+console.log(returnFoo());
 
+
+// Explample 2 
+
+const lastName = 'Bond';
+function greet() {
+    
+    const firstName = 'James';
+    const reply = `The name's ${firstName} ${lastName}.`;
+    return reply;
+
+}
+
+console.log(greet());
+
+// Codepen example 
+
+const counter = ()  => {
+    // Return a function that when invoked increments and returns a counter variable. 
+    // example: const  newCounter = counter();
+    // newCounter(); // 1
+    // newCounter(); // 2
+    
+    let count = 0;
+
+
+}
