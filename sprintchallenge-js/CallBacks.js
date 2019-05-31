4a79897299c04abba6aefbe2021f4a7e521f67dd
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-05-31 11:54:56 -0700

    Commit

---
+++ sprintchallenge-js/CallBacks.js
@@ -0,0 +1,70 @@
+// Callbacks Reviews
+// Demonstrate the proper use of a callback funtion by passing a function to another function as a parameter. 
+// Functions can be passed as parameters to another function.. 
+
+// Example 1
+
+const functionFeeder = function(callback){
+    callback('Hello from inside the Function Feeder');
+};
+
+functionFeeder(function(greeting){
+    console.log(greeting);
+});
+
+// Example 2 
+
+function sayHello(name) {
+    console.log(`Hello, ${name}`);
+}
+
+function callSayHellowWithLars(callback) {
+    const innerName = 'Lars';
+    callback(innerName);
+} 
+
+callSayHellowWithLars(sayHello);
+
+// Example 3 
+
+// Example as a for loop logging out items in an array 
+
+const elements = ['earth','wind','fire','water'];
+const elements1 = ['earth1','wind2','fire3','water4'];
+for(i = 0; i < elements.length; i++) {
+
+    console.log(elements[i]);
+
+}
+
+// As a callback function. 
+
+const newforEach = (list,callback) => {
+
+    for(let i = 0; i < list.length; i++){
+
+        callback(list[i],i)
+
+    }
+
+};
+
+/* Orginal way 
+newforEach(elements, function(item, index) {
+    console.log(item,index);
+});
+
+newforEach(elements1, function(item, index) {
+    console.log(item,index);
+});
+
+*/
+
+// new Interator way.  
+
+const iterator = function(item,index) {
+    console.log(item,index);
+} 
+
+newforEach(elements,iterator);
+newforEach(elements,iterator);
--- sprintchallenge-js/Closures.js
+++ sprintchallenge-js/Closures.js
@@ -28,14 +28,28 @@ function greet() {
 console.log(greet());
 
 // Codepen example 
-
+// Counter function holds onto imcrmented value, stores in memory. 
 const counter = ()  => {
     // Return a function that when invoked increments and returns a counter variable. 
     // example: const  newCounter = counter();
     // newCounter(); // 1
     // newCounter(); // 2
-    
+
     let count = 0;
+    return function() {
+        
+        //count = count + 1;
+
+        return ++count;
+    }
 
 
 }
+
+const newCounter = counter();
+
+console.log(newCounter());
+
+console.log(newCounter());
+
+console.log(newCounter());
