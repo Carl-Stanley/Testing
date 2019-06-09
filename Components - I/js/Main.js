4e73c459b6619c6fcf8c0963724c33b8800f576e
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-09 13:19:57 -0700

    commit

---
+++ Components - I/js/Main.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll('.button');
+
+class Button {
+  constructor(element){
+    this.element = element;
+    this.element.addEventListener('click', () => { this.buttonClick() });
+  }
+  buttonClick(event){
+    //console.log('button clicked!');
+    alert('Mouse attacked me!');
+  } 
+}
+
+/*
+const btn1 = document.getElementsByClassName("button")[0];
+const btn2 = document.getElementsByClassName("button")[1];
+const btn3 = document.getElementsByClassName("button")[2];
+
+btn1.addEventListener('mouseover',function(event){ 
+
+    alert('Mouse attacked me!');
+
+});
+
+// Double Click 
+btn2.addEventListener('dblclick',function(event){ 
+
+    alert('You double clicked me!');
+
+});
+
+// Click 
+btn3.addEventListener('click',function(event){ 
+
+    alert('You clicked me!');
+
+});
+*/
